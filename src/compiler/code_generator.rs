d6282ed2484e55436edebcff154ec1b465c6d4f3
Finn Bear <finnbearlabs@gmail.com>
2025-10-24 01:14:08 -0700

    fmt.

--- src/compiler/code_generator.rs
+++ src/compiler/code_generator.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use crate::{ast::*, bytecode::*, BinaryOperator, Value};
+use std::collections::HashMap;
 use thiserror::Error;
 
 /// Turns an abstract syntax tree into bytecode.
